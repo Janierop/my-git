0cb64ebbba06e1884573aa4ef02be1da1f2a2723
Jaden <jaden.nierop@hotmail.com>
2024-03-19 14:37:26 +0100

    test hash-object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 mod init;
 mod cli;
 mod cat_file;
--- src/test.rs
+++ src/test.rs
@@ -64,4 +64,25 @@ mod test {
         assert_eq!(my_git_output.stderr, git_output.stderr, "Errors do not match");
         assert_eq!(my_git_output.status.success(), git_output.status.success(), "Status codes do not match");
     }
+
+    #[test]
+    fn test_git_hash_object() {
+        let file = "deleteme"; // Or any object hash that you want to test
+
+        let git_output = Command::new(GIT)
+            .args(["hash-object", file])
+            .output()
+            .expect("Failed to execute command");
+
+        let my_git_output = Command::new(MYGIT_PATH)
+            .args(["hash-object", file])
+            .output()
+            .expect("Failed to execute command");
+    
+        // Compare the outputs
+        assert_eq!(my_git_output.stdout, git_output.stdout, "Outputs do not match");
+        assert_eq!(my_git_output.stderr, git_output.stderr, "Errors do not match");
+        assert_eq!(my_git_output.status.success(), git_output.status.success(), "Status codes do not match");
+    }
+
 }
